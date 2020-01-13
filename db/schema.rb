0e5515f8de2c956688865479eb3ebfb7641b3603
mailie cook <mailiecook@yahoo.com>
2020-01-13 13:41:51 -0800

    add views for animals

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200113194623) do
+ActiveRecord::Schema.define(version: 20200113213437) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "animals", force: :cascade do |t|
-    t.string "type"
+    t.string "species"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
